743cfc67b4fe23837428ceb4f48d70d981e237c6
Harsh Bhama <harshbhama96@gmail.com>
2024-03-19 17:14:10 +0530

    integrate redis with rate limiter

--- iternary/controllers/iternaryController.js
+++ iternary/controllers/iternaryController.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const { imageKitMethod } = require("../../utilities/imageKitUpload");
 const { addIternaryDb, listIternaryDb } = require("../db/internaryDb");
 const { RateLimiterMemory } = require("rate-limiter-flexible");
+const { createClient } = require('redis')
+const client = createClient();
+client.connect();
+client.on('error', err => console.log('Redis Client Error', err));
 const opts = {
   points: 6, // Number of Api calls assigned to api route
   duration: 10, // Per second
@@ -14,7 +18,7 @@ router.post('/addIternary', async (req, res) => {
   const userId = req.get("userid")
   const headerValues = JSON.parse(req.get("textProps"))
   const {title, description} = headerValues;
-
+  
   try{
     let result = await imageKitMethod(fileName, fileData)
     await addIternaryDb(title, description, userId, result?.url)
@@ -31,30 +35,38 @@ router.post('/addIternary', async (req, res) => {
 
 })
 router.get("/listIternary", async (req, res) => {
+
+  await client.set('foo', 'bar');
   const userId = req.get("userid");
+
   rateLimiter.consume(userId)
     .then(async (rateLimiterRes) => {
-      console.log("rateLimiterRes",rateLimiterRes)
       try {
         let data = await listIternaryDb(userId);
+        await client.set(userId+'_list_iternary', JSON.stringify(data))
+
         res.json({
           error: false,
           data: data,
           msg: 'Fetched data successfully'
         });
       } catch (err) {
+
         res.json({
           error: true,
           msg: err
         })
       }
     })
-    .catch((rateLimiterRes) => {
+    .catch(async (rateLimiterRes) => {
       // Not enough points to consume
+      let cachedData = JSON.parse(await client.get(userId+'_list_iternary'))
       res.json({
-        error: true,
-        msg: `Rate limit exceeded for this user having user id as ${userId}`
-      })
+        error: false,
+        data: cachedData,
+        msg: `Rate limit exceeded for this user having user id as ${userId}, caching data from redis`
+      });
+
     });
   
 });
